f8ac29be33805921c1a3811f5e7a74cb1f904370
Van40s <vanestojanov49@gmail.com>
2024-12-30 11:14:58 +0100

    removing secrets

--- lab_02_211244/lib/main.dart
+++ lab_02_211244/lib/main.dart
@@ -1,9 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:lab_02_211244/screens/jokes_type_screen.dart'; // Import the home screen
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  dotenv.load();
+  Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MyApp());
 }
+
 
 class MyApp extends StatelessWidget {
 
